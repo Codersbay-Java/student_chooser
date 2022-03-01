24c2af3f1c3d67ced635770bbf8e2dbfd25852f4
Lukas Wais <lukas.wais@outlook.de>
2022-03-01 21:07:38 +0100

    initial

---
+++ android/app/src/main/kotlin/com/example/student_chooser/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.student_chooser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/students.dart
@@ -0,0 +1,17 @@
+class Students {
+  static final List<String> students = [
+    'Alkin Hannes',
+    'Bruckbauer Michael',
+    'Gelbmann Jasmin',
+    'Glass Sebastian',
+    'Hutterer Gregor',
+    'Kaltenberger Astrid',
+    'Lindenhofer Christian',
+    'Manns Stefan',
+    'Nedev Nikolay',
+    'Reichl Elias',
+    'Schmidberger Stephan',
+    'Schmidleitner Michaela',
+    'Weissensteiner Michael',
+  ];
+}
