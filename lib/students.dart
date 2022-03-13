9f29532ab328981ba82b77dc74015831bd456be8
Lukas <lukas.wais@outlook.de>
2022-03-13 10:31:47 +0100

    Add files via upload

---
+++ lib/students.dart
@@ -0,0 +1,11 @@
+class Students {
+  static final List<String> students = [
+    'Alexander Frisch',
+    'Christoph Lagler',
+    'Kilian Dürrschmid',
+    'Maja Miljkovic',
+    'Michael Wagner',
+    'Oliver Straberger',
+    'Theresa Breitwieser',
+  ];
+}
